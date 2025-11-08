340f10d07f1002b0523f00b27e11ce1404b22e18
SRL-Limb-WS4 <climbingroboticsteam@gmail.com>
2025-11-08 19:57:46 +0900

    fix depth frame id and change odom frame id to nav

--- traversability_mapper/launch/madmax_rtabmap.launch.py
+++ traversability_mapper/launch/madmax_rtabmap.launch.py
@@ -11,7 +11,7 @@ def generate_launch_description():
             output='screen',
             parameters=[{
                 'frame_id': 'tcp_base',
-                'odom_frame_id': 'nav_current_submap',
+                'odom_frame_id': 'nav',
                 'subscribe_rgbd': False,
                 'map_frame_id': 'nav',
                 'subscribe_rgbd': False,
@@ -23,8 +23,11 @@ def generate_launch_description():
                 'RGBD/LinearUpdate': '0.1',
                 'RGBD/AngularUpdate': '0.05',
                 'Reg/Strategy': '1',
-                'Vis/MaxFeatures': '1500',
+                'Vis/MaxFeatures': '2000',
+                'RGBD/LoopClosureReextractFeatures': 'true',
+                'Vis/MinInliers': '15',
                 'Optimizer/Strategy': '1',
+                'RGBD/DepthMax': 5.0,
                 'Grid/FromDepth': True,
                 'Grid/RangeMax': '5.0',
                 'qos_odom': 2,
@@ -33,10 +36,10 @@ def generate_launch_description():
                 'qos_camera_info': 2,
             }],
             remappings=[
-                ('/odom', '/nav_current_submap'),
+                ('/odom', '/nav'),
                 ('rgb/image', '/fixed/left/image'),
                 ('rgb/camera_info', '/fixed/left/camera_info'),
-                ('depth/image', '/hcru2/pt_stereo_sgm/depth')
+                ('depth/image', '/fixed/left/depth')
             ],
         ),
 
@@ -48,7 +51,7 @@ def generate_launch_description():
             parameters=[{'use_sim_time': True}],
             remappings=[
                 ('rgb/image', '/fixed/left/image'),
-                ('depth/image', '/hcru2/pt_stereo_sgm/depth'),
+                ('depth/image', '/fixed/left/depth'),
                 ('rgb/camera_info', '/fixed/left/camera_info'),
                 ('odom', 'nav')
             ],
--- traversability_mapper/src/fix_camera_frame.cpp
+++ traversability_mapper/src/fix_camera_frame.cpp
@@ -17,9 +17,14 @@ public:
       "/hcru2/pt_color_rect/left/camera_info", 10,
       std::bind(&FrameIdFixer::infoCallback, this, std::placeholders::_1));
 
+    depth_sub_ = this->create_subscription<sensor_msgs::msg::Image>(
+      "/hcru2/pt_stereo_sgm/depth", 10,
+      std::bind(&FrameIdFixer::depthCallback, this, std::placeholders::_1));
+
     // Publisher
     image_pub_ = this->create_publisher<sensor_msgs::msg::Image>("/fixed/left/image", 10);
     info_pub_ = this->create_publisher<sensor_msgs::msg::CameraInfo>("/fixed/left/camera_info", 10);
+    depth_pub_ = this->create_publisher<sensor_msgs::msg::Image>("/fixed/left/depth", 10);
 
     RCLCPP_INFO(this->get_logger(), "FixCameraFrame node started.");
   }
@@ -39,10 +44,19 @@ private:
     info_pub_->publish(fixed_msg);
   }
 
+  void depthCallback(const sensor_msgs::msg::Image::SharedPtr msg)
+  {
+    auto fixed_msg = *msg;
+    fixed_msg.header.frame_id = "camera_left";
+    depth_pub_->publish(fixed_msg);
+  }
+
   rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
   rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr info_sub_;
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr depth_sub_;
   rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_pub_;
   rclcpp::Publisher<sensor_msgs::msg::CameraInfo>::SharedPtr info_pub_;
+  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr depth_pub_;
 };
 
 int main(int argc, char * argv[])
